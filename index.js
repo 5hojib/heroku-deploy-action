bb930d49c846e67448cbbc361386b2a5b686a5ba
Mehedi Hasan Shojib <yesiamshojib@gmail.com>
2025-02-15 14:27:11 +0600

    Update index.js

--- index.js
+++ index.js
@@ -25,17 +25,16 @@ const createCatFile = ({ email, api_key }) => `cat >~/.netrc <<EOF
 machine api.heroku.com
     login ${email}
     password ${api_key}
-machine git.heroku.co    login ${email}
+machine git.heroku.com
+    login ${email}
     password ${api_key}
 EOF`;
 
-const addRemote = ({ app_name, dontautocreate, buildpack, region, team, stack }) => {
+const addRemote = ({ app_name, buildpack, region, team, stack }) => {
   try {
     execSync("heroku git:remote --app " + app_name);
     console.log("Added git remote heroku");
   } catch (err) {
-    if (dontautocreate) throw err;
-
     execSync(
       "heroku create " +
         app_name +
@@ -47,32 +46,15 @@ const addRemote = ({ app_name, dontautocreate, buildpack, region, team, stack })
   }
 };
 
-const addConfig = ({ app_name, env_file, appdir }) => {
+const addConfig = ({ app_name, appdir }) => {
   let configVars = [];
   for (let key in process.env) {
     if (key.startsWith("HD_")) {
       configVars.push(key.substring(3) + "='" + process.env[key] + "'");
     }
   }
-  if (env_file) {
-    const env = fs.readFileSync(path.join(appdir, env_file), "utf8");
-    const variables = require("dotenv").parse(env);
-    const newVars = [];
-    for (let key in variables) {
-      newVars.push(key + "=" + variables[key]);
-    }
-    configVars = [...configVars, ...newVars];
-  }
   if (configVars.length !== 0) {
     execSync(`heroku config:set --app=${app_name} ${configVars.join(" ")}`);
-  }
-};
-
-const createProcfile = ({ procfile, appdir }) => {
-  if (procfile) {
-    fs.writeFileSync(path.join(appdir, "Procfile"), procfile);
-    execSync(`git add -A && git commit -m "Added Procfile"`);
-    console.log("Written Procfile with custom configuration");
   }
 };
 
@@ -99,16 +81,7 @@ const runCommand = (command, options = {}) => {
   });
 };
 
-const deploy = async ({
-  dontuseforce,
-  app_name,
-  branch,
-  usedocker,
-  dockerHerokuProcessType,
-  dockerBuildArgs,
-  appdir,
-}) => {
-  const force = !dontuseforce ? "--force" : "";
+const deploy = async ({ app_name, branch, usedocker, dockerHerokuProcessType, dockerBuildArgs, appdir }) => {
   if (usedocker) {
     await runCommand(`heroku stack:set container --app ${app_name}`);
     await runCommand(
@@ -132,37 +105,17 @@ const deploy = async ({
     }
 
     if (appdir === "") {
-      await runCommand(`git push heroku ${branch}:refs/heads/main ${force}`, {
+      await runCommand(`git push heroku ${branch}:refs/heads/main --force`, {
         maxBuffer: 104857600,
       });
     } else {
       await runCommand(
-        `git push ${force} heroku \`git subtree split --prefix=${appdir} ${branch}\`:refs/heads/main`,
+        `git push --force heroku \`git subtree split --prefix=${appdir} ${branch}\`:refs/heads/main`,
         { maxBuffer: 104857600 }
       );
     }
   }
   core.setOutput('output', 'Deployment complete.');
-};
-
-const healthcheckFailed = ({
-  rollbackonhealthcheckfailed,
-  app_name,
-  appdir,
-}) => {
-  if (rollbackonhealthcheckfailed) {
-    execSync(
-      `heroku rollback --app ${app_name}`,
-      appdir ? { cwd: appdir } : null
-    );
-    core.setFailed(
-      "Health Check Failed. Error deploying Server. Deployment has been rolled back. Please check your logs on Heroku to try and diagnose the problem"
-    );
-  } else {
-    core.setFailed(
-      "Health Check Failed. Error deploying Server. Please check your logs on Heroku to try and diagnose the problem"
-    );
-  }
 };
 
 // Input Variables
@@ -172,20 +125,10 @@ let heroku = {
   app_name: core.getInput("heroku_app_name"),
   buildpack: core.getInput("buildpack"),
   branch: core.getInput("branch"),
-  dontuseforce: core.getInput("dontuseforce") === "false" ? false : true,
-  dontautocreate: core.getInput("dontautocreate") === "false" ? false : true,
   usedocker: core.getInput("usedocker") === "false" ? false : true,
   dockerHerokuProcessType: core.getInput("docker_heroku_process_type"),
   dockerBuildArgs: core.getInput("docker_build_args"),
   appdir: core.getInput("appdir"),
-  healthcheck: core.getInput("healthcheck"),
-  checkstring: core.getInput("checkstring"),
-  delay: parseInt(core.getInput("delay")),
-  procfile: core.getInput("procfile"),
-  rollbackonhealthcheckfailed:
-    core.getInput("rollbackonhealthcheckfailed") === "false" ? false : true,
-  env_file: core.getInput("env_file"),
-  justlogin: core.getInput("justlogin") === "false" ? false : true,
   region: core.getInput("region"),
   stack: core.getInput("stack"),
   team: core.getInput("team"),
@@ -218,13 +161,6 @@ if (heroku.dockerBuildArgs) {
     // Check if Heroku is installed
     isHerokuInstalled();
 
-    // Just Login
-    if (heroku.justlogin) {
-      execSync(createCatFile(heroku));
-      console.log("Created and wrote to ~/.netrc");
-      return;
-    }
-
     execSync(`git config user.name "Heroku-Deploy"`);
     execSync(`git config user.email "${heroku.email}"`);
     const status = execSync("git status --porcelain").toString().trim();
@@ -248,8 +184,6 @@ if (heroku.dockerBuildArgs) {
     execSync(createCatFile(heroku));
     console.log("Created and wrote to ~/.netrc");
 
-    createProcfile(heroku);
-
     if (heroku.usedocker) {
       await runCommand("heroku container:login");
     }
@@ -258,31 +192,7 @@ if (heroku.dockerBuildArgs) {
     addRemote(heroku);
     addConfig(heroku);
 
-    try {
-      await deploy({ ...heroku, dontuseforce: true });
-    } catch (err) {
-      console.error(`
-            Unable to push branch because the branch is behind the deployed branch. Using --force to deploy branch. 
-            (If you want to avoid this in the future, disable force pushing by setting the dontuseforce option).
-            `);
-      await deploy(heroku);
-    }
-
-    if (heroku.healthcheck) {
-      await sleep(heroku.delay);
-      let res;
-      try {
-        res = await p({ url: heroku.healthcheck });
-      } catch (err) {
-        return healthcheckFailed(heroku);
-      }
-      if (
-        res.statusCode != 200 ||
-        (heroku.checkstring && !res.body.includes(heroku.checkstring))
-      ) {
-        return healthcheckFailed(heroku);
-      }
-    }
+    await deploy(heroku);
   } catch (error) {
     core.setFailed(error.message);
   }
